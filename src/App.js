498d750ed9f711a012117a3982cb374e25139b7f
Håkon Pelsholen Busterud <hakopb@netcompany.com>
2022-05-05 10:25:38 +0200

    Set up routing

--- src/App.js
+++ src/App.js
@@ -4,24 +4,22 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/">Main</Link> |{" "}
+        <Link to="/invoices">Invoices</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
